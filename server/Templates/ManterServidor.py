f71064e3fd310057bf71be53a4568952ae3a7c9e
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-03-09 19:56:34 +0000

    corrigindo manterservidor

--- server/Templates/ManterServidor.py
+++ server/Templates/ManterServidor.py
@@ -5,15 +5,13 @@ class Servidor:
         st.header("Dados do servidor:")
         for _, linha in df.iterrows():
             with st.container(border=True):
-                with st.expander("Servidor"):
+                with st.expander("TCP"):
+                    TCP = "192.168.1.100:5050"
                     st.write(f"Ativo: {True}")
+                    st.write(f"TCP: {TCP}")
                     
                 with st.expander("Broadcast"):
-                    broadcast = "255.255.255.255"
+                    broadcast = "255.255.255.255:5000"
                     st.write(f"Ativo: {True}")
                     st.write(f"Broadcast: {broadcast}")
             
-                with st.expander("TCP"):
-                    TCP = "192.168.1.100"
-                    st.write(f"Ativo: {True}")
-                    st.write(f"TCP: {TCP}")
